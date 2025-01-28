f254529f8520b27ec08023362f5f472bf59dd18b
lowercasebtw <jejojora2@gmail.com>
2025-01-28 00:55:28 -0500

    Placeholder shell.

---
+++ README.md
@@ -0,0 +1,8 @@
+# LightConfig
+
+W.I.P - Work in progress, not done.
+Join our discord: https://discord.gg/U48eDmst68
+
+## License
+
+This project is licensed under the LGPL-3 license.
---
+++ src/main/java/btw/mixces/lightconfig/LightConfigClient.java
@@ -0,0 +1,9 @@
+package btw.mixces.lightconfig;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightConfigClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
